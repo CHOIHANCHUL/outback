f9312480602ae683f73473d5637b202d65f319fe
CHOIHANCHUL <107171312+CHOIHANCHUL@users.noreply.github.com>
2022-06-30 11:11:26 +0900

    Add files via upload

---
+++ js/basic.js
@@ -0,0 +1,25 @@
+const elMmenu = document.querySelectorAll('#gnb > ul > li > a'); 
+const elBar = document.querySelector('.bar');
+
+console.log(elMmenu);
+
+elMmenu.forEach(function(bb){
+    bb.onmouseover = function(){
+        elBar.style = `width:${this.offsetWidth}px; 
+                    left:${this.offsetLeft}px ` 
+    }
+    bb.onmouseout = function(){
+        elBar.style = `width:0px; 
+                    left:${this.offsetLeft}px ` 
+    }
+})
+
+$('#gnb > ul > li').on("mouseenter",function(){
+    //console.log("마우스위에 올려놨어요!!");
+    $(this).find('.sub').stop().fadeIn(500);
+})
+
+$('#gnb > ul > li').on("mouseleave",function(){
+    $(this).find('.sub').stop().fadeOut(100);
+})
+
